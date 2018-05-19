24912eda1c6759814cb3e7a2e877435ead88e48d
Iustin Nita <iustin.nitza@gmail.com>
2018-05-19 14:15:46 +0200

    Change ball color every time it bounces off a wall

--- game.js
+++ game.js
@@ -6,12 +6,15 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+let colors = ["red", "green", "yellow", "teal", "pink", "magenta"];
+
 let ballRadius = 10;
+let ballColor = colors[getRandomInt(0, colors.length)];
 
 function drawBall() {
     context.beginPath();
     context.arc(x, y, ballRadius, 0, Math.PI * 2);
-    context.fillStyle = "#0095DD";
+    context.fillStyle = ballColor;
     context.fill();
     context.closePath();
 }
@@ -26,11 +29,22 @@ function draw() {
 
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
+        ballColor = colors[getRandomInt(0, colors.length)];
+
     }
 
     if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         dy = -dy;
+        ballColor = colors[getRandomInt(0, colors.length)];
     }
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 setInterval(draw, 10);
