75fb0bf3538559a4bee41fb71371528a7b8b7f3d
Iustin Nita <iustin.nitza@gmail.com>
2018-05-19 14:01:44 +0200

    Add wall collision!

--- game.js
+++ game.js
@@ -6,18 +6,31 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
-function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    
+let ballRadius = 10;
+
+function drawBall() {
     context.beginPath();
-    context.arc(x, y, 10, 0, Math.PI * 2);
+    context.arc(x, y, ballRadius, 0, Math.PI * 2);
     context.fillStyle = "#0095DD";
     context.fill();
     context.closePath();
+}
+
+function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    drawBall();
     
     x += dx;
     y += dy;
-    
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
 }
 
 setInterval(draw, 10);
